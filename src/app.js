302115898d380a0dd4994a12cde39fd5db14567c
Mai Tri Toan <69956369+likeoneringfake@users.noreply.github.com>
2021-09-07 14:17:57 +0700

    destroy morgan

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express')
-const morgan = require('morgan')
+// const morgan = require('morgan')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 // Route
@@ -25,7 +25,7 @@ app.use(
 )
 app.use(cookieParser())
 
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 app.use(
 	cors({
